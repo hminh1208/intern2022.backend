e8ec075a5ea3fddea48e0bba025e3d78377a76af
Hong Minh - VNPT IT3 <hminh1208@gmail.com>
2022-07-22 16:06:02 +0700

    *feat: cập nhật City, bổ sung mapper

---
+++ Extensions/MappingProfiles.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using WebApi.Models.Cities;
+
+namespace WebApi.Extensions
+{
+    public class MappingProfiles: Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<City, CityResponseDto>();
+        }
+    }
+}
