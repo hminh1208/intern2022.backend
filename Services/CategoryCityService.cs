e5aff7614f2197d8da1051f0deb717c948cba50a
Nguyen Sinh Hung <85824386+hungns021001@users.noreply.github.com>
2022-08-01 09:51:33 +0700

    Feat thêm ngoại lệ của edit và thêm get (#7)

--- Services/CategoryCityService.cs
+++ Services/CategoryCityService.cs
@@ -8,9 +8,10 @@ namespace WebApi.Services
         Task<List<CategoryCity>> GetAll();
         Task<CategoryCity> GetById(int id);
         Task<CategoryCity> AddCategoryCity(CategoryCityDto categoryCityDto);
-        Task<CategoryCity> EditCategoryCity(int id, CategoryCityDto categoryCityDtos);
+        Task<CategoryCity> EditCategoryCity(int id, CategoryCityDto categoryCityDto);
         Task<CategoryCity> DeleteCategoryCity(int id);
-
+        Task<List<CategoryCity>> GetCategoryCityActive();
+        Task<List<CategoryCity>> GetCategoryCityDeleted();
     }
     public class CategoryCityService : ICategoryCityService
     {
@@ -47,7 +48,6 @@ namespace WebApi.Services
             if (newCategoryCity != null)
             {
                 newCategoryCity.Status = (int)StatusEnum.DELETED;
-
                 this.dataContext.Entry(newCategoryCity).State = EntityState.Modified;
                 await this.dataContext.SaveChangesAsync();
             }
@@ -57,7 +57,8 @@ namespace WebApi.Services
         public async Task<CategoryCity> EditCategoryCity(int id, CategoryCityDto categoryCityDto)
         {
             CategoryCity newCategoryCity = await this.GetById(id);
-            if (newCategoryCity != null)
+
+            if (newCategoryCity != null && newCategoryCity.Status == 0)
             {
                 newCategoryCity.Name = categoryCityDto.Name;
                 newCategoryCity.ShortName = categoryCityDto.ShortName;;
@@ -76,6 +77,16 @@ namespace WebApi.Services
         public async Task<CategoryCity> GetById(int id)
         {
             return dataContext.CategoryCities.FirstOrDefault(c => c.Id == id);
+        }
+
+        public async Task<List<CategoryCity>> GetCategoryCityActive()
+        {
+            return dataContext.CategoryCities.Where(c => c.Status == (int)StatusEnum.DRAFT).ToList();
+        }
+
+        public async Task<List<CategoryCity>> GetCategoryCityDeleted()
+        {
+            return dataContext.CategoryCities.Where(c => c.Status == (int)StatusEnum.DELETED).ToList();
         }
     }
 }
